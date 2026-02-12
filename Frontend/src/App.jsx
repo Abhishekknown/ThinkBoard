d0de2023eed472500d1d05baff0157970983b71c
Your Name <90549183+Abhishekknown@users.noreply.github.com>
2026-02-12 10:32:08 +0530

    fix case-sensitive import

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
-import CreatePage from "./pages/Createpage";
+import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
 import toast from "react-hot-toast";
 
